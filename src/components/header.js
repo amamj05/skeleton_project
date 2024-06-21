6ffe7fdbc027e34b492d5b7754f5360312aef930
mj <ui2939@gmail.com>
2024-06-21 20:48:05 +0900

    edit header emoji error

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import happiness from '../assets/face-emoji-happiness.png'
 
 
 export default function Header(props) {
-    let useImg = <img src={props.emoji} className='header-emoji' alt='useImg'></img>
+
+    let useImg = props.emoji ? <img src={props.emoji} className='header-emoji' alt='useImg'></img>
+                            :<img src={happiness} className='header-emoji' alt='useImg'></img>;
+ 
     return (
         <div className='main-header'>
             <input className='textInput'></input>
