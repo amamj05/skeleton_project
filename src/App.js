fa5ecd0ec8af4c2ede892b81f448f794eb5f58c2
mj <ui2939@gmail.com>
2024-06-21 15:59:36 +0900

    edit

--- src/App.js
+++ src/App.js
@@ -13,28 +13,26 @@ import trip from './img/trip.png';
 const importAll = (requireContext) => requireContext.keys().map(requireContext);
 
 const imagePath = importAll(
-  require.context('./img', true, /\.(png|jpe?g|svg)$/)
+    require.context('./img', true, /\.(png|jpe?g|svg)$/)
 );
 
 
 
 function Home(props) {
-    
     let lis = [];
 
-
-
     for (let i = 0; i < imagePath.length; i++) {
-        if (imagePath[i].substr(14, 10) === 'face-emoji'){
+        if (imagePath[i].substr(14, 10) === 'face-emoji') {
 
-        lis.push(<li key={i}><NavLink to="/homelist" >
-            <img src={imagePath[i]} id={i} className='home-emoji' onClick={()=>{
-                console.log(imagePath[i]);
-                props.setEmoji(imagePath[i]);}}></img>
+            lis.push(<li key={i}><NavLink to="/homelist" >
+                <img src={imagePath[i]} id={i} className='home-emoji' onClick={() => {
+                    props.setEmoji(imagePath[i]);
+                }}></img>
             </NavLink></li>)
-    }}
+        }
+    }
 
-   
+
     return (
         <div className='skeleton-home'>
             <h1>오늘의 기분을 골라주세요</h1>
@@ -48,15 +46,15 @@ function Home(props) {
 }
 
 function Header(props) {
-    
-    
+
+
 
 
     let useImg = <img src={props.emoji} className='header-emoji'></img>
     return (
         <div className='skeleton-header'>
             <input className='textInput'></input>
-        {useImg}
+            {useImg}
         </div>
     )
 }
@@ -64,23 +62,23 @@ function Header(props) {
 
 
 function HomeList(props) {
-    const program = [{ id: 1, class:"meditation", img:<img src={meditation} className='contentsbox-list'/>},
-        { id: 2, class:"nature", img:<img src={nature} className='contentsbox-list'/>},
-        { id: 3, class:"gathering", img:<img src={gathering} className='contentsbox-list'/>},
-        { id: 4, class:"test", img:<img src={test} className='contentsbox-list'/>},
-        { id: 5, class:"sleep", img:<img src={sleep} className='contentsbox-list'/>},
-        { id: 6, class:"trip", img:<img src={trip} className='contentsbox-list'/>}
+    const program = [{ id: 1, class: "meditation", img: <img src={meditation} className='contentsbox-list' /> },
+    { id: 2, class: "nature", img: <img src={nature} className='contentsbox-list' /> },
+    { id: 3, class: "gathering", img: <img src={gathering} className='contentsbox-list' /> },
+    { id: 4, class: "test", img: <img src={test} className='contentsbox-list' /> },
+    { id: 5, class: "sleep", img: <img src={sleep} className='contentsbox-list' /> },
+    { id: 6, class: "trip", img: <img src={trip} className='contentsbox-list' /> }
     ];
 
-    
+
     let lis = [];
     for (let i = 1; i < 7; i++) {
-        lis.push(<li key={i}><NavLink to={"/contentslist/" + i}>{program[i-1].img}</NavLink></li>);
+        lis.push(<li key={i}><NavLink to={"/contentslist/" + i}>{program[i - 1].img}</NavLink></li>);
     }
     console.log(props.id);
     return (
         <div>
-            <Header emoji={ props.emoji }></Header>
+            <Header emoji={props.emoji}></Header>
             <div className='skeleton-homelist'>
                 {lis}
             </div>
@@ -89,25 +87,26 @@ function HomeList(props) {
 }
 
 function Contents(props) {
-    const program = [{ id: 1, class:"meditation", img:<img src={meditation} className='card-box'/>},
-        { id: 2, class:"nature", img:<img src={nature} className='card-box'/>},
-        { id: 3, class:"gathering", img:<img src={gathering} className='card-box'/>},
-        { id: 4, class:"test", img:<img src={test} className='card-box'/>},
-        { id: 5, class:"sleep", img:<img src={sleep} className='card-box'/>},
-        { id: 6, class:"trip", img:<img src={trip} className='card-box'/>}
+    const program = [{ id: 1, class: "meditation", img: <img src={meditation} className='card-box' /> },
+    { id: 2, class: "nature", img: <img src={nature} className='card-box' /> },
+    { id: 3, class: "gathering", img: <img src={gathering} className='card-box' /> },
+    { id: 4, class: "test", img: <img src={test} className='card-box' /> },
+    { id: 5, class: "sleep", img: <img src={sleep} className='card-box' /> },
+    { id: 6, class: "trip", img: <img src={trip} className='card-box' /> }
     ];
-    const {num} = useParams();
+    const { num } = useParams();
     let lis = [];
     for (let i = 0; i < 6; i++) {
-        if(num == program[i].id){
-        lis.push(<article key={i+1}>{program[i].img}</article>);
-    }}
+        if (num == program[i].id) {
+            lis.push(<article key={i + 1}>{program[i].img}</article>);
+        }
+    }
 
     return (
         <div>
-            <Header emoji={ props.emoji }></Header>
+            <Header emoji={props.emoji}></Header>
             <div className='contents-box'>
-            {lis}
+                {lis}
             </div>
         </div>
     )
@@ -121,7 +120,7 @@ function App() {
             <Routes>
                 <Route path='/' element={<Home setEmoji={setEmoji} />}></Route>
                 <Route path='/homelist' element={<HomeList emoji={Emoji} />}></Route>
-                <Route path='/contentslist/:num' element={<Contents  emoji={Emoji} />}></Route>
+                <Route path='/contentslist/:num' element={<Contents emoji={Emoji} />}></Route>
             </Routes>
         </div>
 
